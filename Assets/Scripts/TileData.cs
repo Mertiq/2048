e97feb2219ee52f92290f4df7ae3a25b3107ed4d
Mert İshak Kılıç <mertishakilic@gmail.com>
2023-08-08 01:33:18 +0300

    tile data generated

---
+++ Assets/Scripts/TileData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TileData", fileName = "TileData")]
+public class TileData : ScriptableObject
+{
+    public Color bgColor;
+    public Color textColor;
+}
